a117d0e8e04c64a46ada3bd6ef98923bc4ace7e6
Gunnar Lund <gunnar.l.n@gmail.com>
2019-11-04 23:35:38 -0500

    random edits

--- handout.sh
+++ handout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc ~/Projects/pandoc-templates/handout.yaml $1 -o $2 -H ~/Projects/pandoc-templates/header-handout.tex --filter pandoc-citeproc --lua-filter ~/Projects/pandoc-templates/lingpan.lua --lua-filter ~/Projects/pandoc-templates/gnl.lua
+pandoc ~/Projects/pandoc-templates/handout.yaml $1 -o $2 -H ~/Projects/pandoc-templates/handout-header.tex --filter pandoc-citeproc --lua-filter ~/Projects/pandoc-templates/lingpan.lua --lua-filter ~/Projects/pandoc-templates/gnl.lua
